bd7b130f3440dedabb3423ed2a218f94a4990ce4
Lizka03 <kobzeva0303@bk.ru>
2022-09-25 23:02:29 +0300

    Lab1_2
    Добавила функцию, для ввода параметров трубы

--- KobzevaLab1/KobzevaLab1.cpp
+++ KobzevaLab1/KobzevaLab1.cpp
@@ -9,18 +9,27 @@ struct CS {
     int  Workshop = 0, WorkingWorkshop = -1;
     float Efficiency = 0;
 };
-
+Pipe NewPipe() {
+    Pipe newP;
+    cout << "\nEnter pipe length:\n";
+    cin >> newP.Lenght;
+    cout << "\nEnter pipe diameter:\n";
+    cin >> newP.Diameter;
+    cout << "\nEnter 1 if the pipe is working /n Enter 2 if the pipe is being repaired:\n";
+    cin >> newP.Status;
+    return newP;
+}
 int main() { 
     CS cs;
     Pipe p;
     int choice = -1;
 
     while (choice) {
-        cout << "    Menu\n 1. Add pipe\n 2. Add CS\n 3. View all objects\n 4. Edit pipe\n 5. Edit CS\n 6. Save\n 7. Load\n 0. Exit";
+        cout << "    Menu\n 1. Add pipe\n 2. Add CS\n 3. View all objects\n 4. Edit pipe\n 5. Edit CS\n 6. Save\n 7. Load\n 0. Exit\n";
         cin >> choice;
         switch (choice) {
         case 1:
-
+            p = NewPipe();
             break;
         case 2:
 
