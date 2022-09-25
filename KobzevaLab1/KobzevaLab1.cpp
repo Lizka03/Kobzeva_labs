432f3bc5b73bcaf8a64fc0af06d0d8cd6cc894d2
Lizka03 <kobzeva0303@bk.ru>
2022-09-25 22:47:42 +0300

    Lab1_1

---
+++ KobzevaLab1/KobzevaLab1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+struct Pipe {
+    float Lenght = 0, Diameter = 0;
+    int Status = -1;
+};
+struct CS {
+    string Name = "";
+    int  Workshop = 0, WorkingWorkshop = -1;
+    float Efficiency = 0;
+};
+
+int main() { 
+    CS cs;
+    Pipe p;
+    int choice = -1;
+
+    while (choice) {
+        cout << "    Menu\n 1. Add pipe\n 2. Add CS\n 3. View all objects\n 4. Edit pipe\n 5. Edit CS\n 6. Save\n 7. Load\n 0. Exit";
+        cin >> choice;
+        switch (choice) {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 6:
+
+            break;
+        case 7:
+
+            break;
+        }
+    }
+}
